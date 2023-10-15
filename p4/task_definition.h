ad30fff69da05e6bc80c79f37245c903b87c358d
yjmantilla <yjmantilla@gmail.com>
2023-10-15 00:53:53 -0500

    reorganization

---
+++ p4/task_definition.h
@@ -0,0 +1,17 @@
+/*************************************************/
+//  - Code by Prof. Gustavo Patino  -
+//    - Real-Time Systems (IEO 2547027) -
+//     - University of Antioquia (UdeA) -
+//      - Medellin, Colombia 2023 -
+/*************************************************/
+
+#include <stdio.h>
+#include <pthread.h>
+
+#ifndef LIB_HPP
+#define LIB_HPP
+
+void thread_1(int* c);
+void thread_2(int* c);
+
+#endif
