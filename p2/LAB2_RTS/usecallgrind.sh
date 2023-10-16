65e47d5b8e2a30f21685c8682c7e5f030bcc7f39
yjmantilla <yjmantilla@gmail.com>
2023-10-15 22:55:47 -0500

    callgrind

--- p2/LAB2_RTS/usecallgrind.sh
+++ p2/LAB2_RTS/usecallgrind.sh
@@ -1,5 +1,5 @@
 # Run
-valgrind --tool=callgrind --dump-instr=yes --cache-sim=yes --instr-atstart=no --callgrind-out-file=Linear.grind ./Linear 
-valgrind --tool=callgrind --dump-instr=yes --cache-sim=yes --instr-atstart=no --callgrind-out-file=Random.grind ./Random
+valgrind --tool=callgrind --dump-instr=yes --cache-sim=yes --instr-atstart=no --callgrind-out-file=Linear.callgrind ./Linear 
+valgrind --tool=callgrind --dump-instr=yes --cache-sim=yes --instr-atstart=no --callgrind-out-file=Random.callgrind ./Random
 
-# Then use kcachegrind callgrind.out.<pid> or the .grind, opens a gui, then go to the Call Graph tab in the bottom right corner
+# Then use kcachegrind callgrind.out.<pid> or the .callgrind, opens a gui, then go to the Call Graph tab in the bottom right corner
