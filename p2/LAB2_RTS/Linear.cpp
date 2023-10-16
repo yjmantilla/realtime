3a30627f8aa0106615e3d96555ac1d585db2a7bd
yjmantilla <yjmantilla@gmail.com>
2023-10-15 23:10:33 -0500

    new funcs

--- p2/LAB2_RTS/Linear.cpp
+++ p2/LAB2_RTS/Linear.cpp
@@ -6,7 +6,7 @@
 #define SIZE 10485760
 #define NBR 10000
 
-int randomAccessSum(int* a, int* index) {
+int indexedAccessSum(int* a, int* index) {
     int sum = 0;
     for(int n = 0; n < NBR; ++n)
         sum += a[index[n]];
@@ -20,12 +20,19 @@ void fillArray(int* a, int size) {
 
 void linearIndexes(int* index, int numreads) {
     for(int k = 0; k < numreads; ++k)
-        index[k] = k;
+        index[k] = k; //rand() % SIZE;
+}
+
+int iterateSums(int* a, int* index) {
+    int d = 0;
+    for(int k = 0; k < 1000; ++k) {
+        d += indexedAccessSum(a, index);
+    }
+    return d;
 }
 
 int main() {
     CALLGRIND_START_INSTRUMENTATION;    
-// array of data
     int* a = new int[SIZE];
     fillArray(a, SIZE);
 
@@ -33,11 +40,7 @@ int main() {
     int* index = new int[NBR];
     linearIndexes(index, NBR);
 
-    int d = 0;
-    for(int k = 0; k < 1000; ++k) {
-        d+=randomAccessSum(a, index);
-    }
-
+    int d = iterateSums(a, index);
     printf("%d\n", d);
     CALLGRIND_STOP_INSTRUMENTATION;
     return 0;
--- p2/LAB2_RTS/Random.cpp
+++ p2/LAB2_RTS/Random.cpp
@@ -6,7 +6,7 @@
 #define SIZE 10485760
 #define NBR 10000
 
-int randomAccessSum(int* a, int* index) {
+int indexedAccessSum(int* a, int* index) {
     int sum = 0;
     for(int n = 0; n < NBR; ++n)
         sum += a[index[n]];
@@ -18,28 +18,30 @@ void fillArray(int* a, int size) {
         a[i] = i;
 }
 
-void randomIndexes(int* index, int numreads) {
+void linearIndexes(int* index, int numreads) {
     for(int k = 0; k < numreads; ++k)
-        index[k] = rand() % SIZE;
+        index[k] = rand() % SIZE; //k;
+}
+
+int iterateSums(int* a, int* index) {
+    int d = 0;
+    for(int k = 0; k < 1000; ++k) {
+        d += indexedAccessSum(a, index);
+    }
+    return d;
 }
 
 int main() {
     CALLGRIND_START_INSTRUMENTATION;    
-// array of data
     int* a = new int[SIZE];
     fillArray(a, SIZE);
 
     // array of random index
     int* index = new int[NBR];
-    randomIndexes(index, NBR);
+    linearIndexes(index, NBR);
 
-    int d = 0;
-    for(int k = 0; k < 1000; ++k) {
-        d+=randomAccessSum(a, index);
-    }
-
+    int d = iterateSums(a, index);
     printf("%d\n", d);
     CALLGRIND_STOP_INSTRUMENTATION;
     return 0;
 }
-
