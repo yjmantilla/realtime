04fcaa1100e2088f99995e44dc64053141a3f153
yjmantilla <yjmantilla@gmail.com>
2023-10-16 00:48:38 -0500

    msprint line

--- p2/LAB2_RTS/memusage.sh
+++ p2/LAB2_RTS/memusage.sh
@@ -1,3 +1,4 @@
 valgrind --tool=massif --peak-inaccuracy=0.0 --stacks=yes --heap=yes --time-unit=ms --detailed-freq=1 --threshold=0.0 --massif-out-file=Linear.massif ./Linear
 valgrind --tool=massif --peak-inaccuracy=0.0 --stacks=yes --heap=yes --time-unit=ms --detailed-freq=1 --threshold=0.0 --massif-out-file=Random.massif ./Random
 # Use massif-visualizer massif.out.XXXX or the .massif file above
+# and ms_print massif.out.XXXX or the .massif file above
