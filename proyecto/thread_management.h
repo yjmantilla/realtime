71714323e17a4a3822fa1d531e36762714faec79
yjmantilla <yjmantilla@gmail.com>
2023-12-01 16:01:05 -0500

    minor comments

--- proyecto/thread_management.h
+++ proyecto/thread_management.h
@@ -16,10 +16,10 @@ typedef struct {
     int wcet_ms;               // Worst-case execution time in milliseconds
     pthread_mutex_t* mutex;    // Optional mutex for shared resource
     task_code task_function;   // Function pointer to the task-specific code
-    int log;
-    int offset; // from the start
-    struct timespec nextActivation;
-    float overheadOffset; // amount of ms to correct (substract) the next activation to account for logging overhead
+    int log; // 1 | 0 to log to files or not (less overhead)
+    int offset; // offeset from the start, not really used
+    struct timespec nextActivation; // variable to save the next activation of the task
+    float overheadOffset; // amount of ms to correct (substract) from the the next activation to account for logging overhead
     float deadlineTolerance; //amount of milliseconds to consider a deadline miss
 } task_t;
 
